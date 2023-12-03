58f96c5e62faa9e00fb0aa9ba93957d2f0c2c190
Anulekha Koley <127935844+anulekha305@users.noreply.github.com>
2023-12-03 21:56:16 +0530

    Update indextour.js

--- indextour.js
+++ indextour.js
@@ -1,14 +1,4 @@
-// let user =document.getElementById("i1");
 
-//  user.addEventListener("click",()=>{
-//  document.querySelector(".loginpage").classList.toggle(".active1");
-// })
-
- 
-// // let btn=document.querySelector("#i1");
-// // btn.addEventListener("click",()=>{
-// //     console.log(".formpopup");
-// // })
 let placename=document.getElementById("placename");
 let submit=document.getElementById("submit");
 submit.addEventListener("click",function(){
